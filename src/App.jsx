27cf4ba0f3d6eb8d99f71991367a55fd78382a47
Henrik Kiepe <hkiepe@inkontor.com>
2023-03-27 21:25:08 +0200

    created routes and loader

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// rrd imports
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Routes
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    loader: dashboardLoader,
+  },
+  {
+    path: "/about",
+    element: <h1>About</h1>,
+  },
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,22 @@
+// rrd imports
+import { useLoaderData } from "react-router-dom";
+
+// helper functions
+import { fetchData } from "../helpers";
+
+export function dashboardLoader() {
+  const userName = fetchData("userName");
+  return { userName };
+}
+
+const Dashboard = () => {
+  const { userName } = useLoaderData();
+
+  return (
+    <div>
+      <h1>{userName}</h1>Dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
